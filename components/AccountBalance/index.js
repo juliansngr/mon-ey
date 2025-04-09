8023690a7384ca6a64f385991ab137dd1ba868ec
Bjoern <beck.b.hh@gmail.com>
2025-04-09 15:01:25 +0200

    refactor: improve filter handling and show filtered list  in AccountBalance 🛠️

--- components/AccountBalance/index.js
+++ components/AccountBalance/index.js
@@ -1,3 +1,4 @@
+import { useTransactionsContext } from "@/utils/TransactionsContext/TransactionsContext";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 
@@ -6,6 +7,7 @@ let totalBalance = 0, totalIncome = 0, totalExpenses = 0;
 export default function AccountBalance({ transactions }) {
   const [isNegative, setIsNegative] = useState(false);
   const [activeButton, setActiveButton] = useState(0);
+  const { handleFilterChange } = useTransactionsContext();
 
   totalBalance = 0;
   totalIncome = 0;
@@ -28,6 +30,13 @@ export default function AccountBalance({ transactions }) {
   }, []);
 
   const handleToggle = (btnIndex) => {
+    if (btnIndex === 0) {
+      handleFilterChange({ type: null, pattern: null });
+    } else if (btnIndex === 1) {
+      handleFilterChange({ type: "type", pattern: "income" });
+    } else if (btnIndex === 2) {
+      handleFilterChange({ type: "type", pattern: "expense" });
+    }
     setActiveButton(btnIndex);
   };
 
@@ -36,7 +45,9 @@ export default function AccountBalance({ transactions }) {
       <StyledAccount
         className={activeButton === 0 ? "active" : ""}
         onClick={() => handleToggle(0)}
-        aria-label="Gesamt Kontostand anzeigen" title="Gesamt Kontostand anzeigen"
+        aria-label="Gesamt Kontostand anzeigen"
+        aria-expanded={activeButton === 0 ? "true" : "false"}
+        title="Gesamt Kontostand anzeigen"
       >
         <StyledTotalAmount $isNegative={isNegative}>
           <StyledTotalSpan>Aktueller Kontostand</StyledTotalSpan>
@@ -48,7 +59,9 @@ export default function AccountBalance({ transactions }) {
         <StyledAccount
           className={activeButton === 1 ? "active" : ""}
           onClick={() => handleToggle(1)}
-          aria-label="Gesamt Eingänge anzeigen" title="Gesamt Eingänge anzeigen"
+          aria-label="Gesamt Eingänge anzeigen"
+          aria-expanded={activeButton === 1 ? "true" : "false"}
+          title="Gesamt Eingänge anzeigen"
         >
           <StyledTotalAmount>
             <StyledTotalSpan>Gesamt Eingang</StyledTotalSpan>
@@ -59,7 +72,9 @@ export default function AccountBalance({ transactions }) {
         <StyledAccount
           className={activeButton === 2 ? "active" : ""}
           onClick={() => handleToggle(2)}
-          aria-label="Gesamt Ausgänge anzeigen" title="Gesamt Ausgänge anzeigen"
+          aria-label="Gesamt Ausgänge anzeigen"
+          aria-expanded={activeButton === 1 ? "true" : "false"}
+          title="Gesamt Ausgänge anzeigen"
         >
           <StyledTotalAmount $isNegative={true}>
             <StyledTotalSpan>Gesamt Ausgang</StyledTotalSpan>
@@ -77,20 +92,22 @@ const StyledAccount = styled.button`
   margin: 0 0 var(--md);
   padding: var(--3xs);
   border-radius: var(--xs);
-  background-color: white;
+  background-color: var(--green-50);
   border: var(--3xs) solid transparent;
   box-shadow: var(--box-shadow-default);
-  transition: background-color 0.3s ease, transform 0.3s ease;
+  transition: background-color 0.6s ease, transform 0.8s ease;
 
   &:hover {
     transform: scale(1.1);
     background-color: var(--green-200);
     cursor: pointer;
+    box-shadow: var(--box-shadow-active);
   }
 
   &.active {
     background-color: var(--green-500);
     border: var(--3xs) solid var(--darkgray);
+    box-shadow: var(--box-shadow-active);
   }
 
   &:last-child.active {
