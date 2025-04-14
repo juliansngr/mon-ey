7cb4776677d38c3e5de09c900b671f62ef7d0e45
Bjoern <beck.b.hh@gmail.com>
2025-04-14 08:36:14 +0200

    refactor: Remove extra space in main closing tag ✨

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }) {
           <ModalProvider>
             <Component {...pageProps} />
           </ModalProvider>
-        </main >
+        </main>
         <Navigation />
       </TransactionsProvider>
     </>
